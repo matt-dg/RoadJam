52aa0cb9f09a070a7b2a0d2c2da4c83faff6584e
Andrea <andry.altomonte@gmail.com>
2018-10-16 15:37:41 +0200

    new

--- RoadJam/ViewController.swift
+++ RoadJam/ViewController.swift
@@ -14,7 +14,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func button(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var text: UITextField!
+    
+    
+    
 }
 
