465a2573da51dc9b55348eefa2acd0194d889569
Julia <juliatsoi1106@gmail.com>
2018-10-16 17:16:56 +0200

    new

---
+++ RoadJam/EventTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventTableViewCell.swift
+//  RoadJam
+//
+//  Created by Tsoi Julia on 16/10/2018.
+//  Copyright © 2018 BlueNote. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
